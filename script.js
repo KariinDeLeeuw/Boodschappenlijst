22253495d059c633f1ac1866b23384d10b5453a3
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2021-05-16 15:14:33 +0200

    for loop toevoegen

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
 var boodschappentas = [];
-function gaBoodschappenDoen() {
+
+function gaBoodschappenDoen(){
   var item = document.getElementById("boodschapitem").value;
   boodschappentas.push(item);
   console.log(boodschappentas);
+  toonBoodschappen();
+}
+
+function toonBoodschappen(){
+  var htmlString = "test";
+  for(var x = 0 ; x < boodschappentas.lenght ; x++ ){
+    htmlString += boodschappentas[x];
+  }
+  var deLijst = document.getElementById("lijst");
+  deLijst.innerHTML = htmlString;
 }
