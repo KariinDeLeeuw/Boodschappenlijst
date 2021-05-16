7bf35682ba896eab6ef38c08729b73b282bfd1da
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2021-05-16 14:29:35 +0200

    Een array toegevoegd

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+var boodschappentas = [];
 function gaBoodschappenDoen() {
-  
+  var item = document.getElementById("boodschapitem").value;
+  boodschappentas.push(item);
+  console.log(boodschappentas);
 }
