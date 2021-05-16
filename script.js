f97c8cac6b04ef6f6ce23c81e716080875c09dc2
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2021-05-16 16:12:27 +0200

    optie verwijderen toegevoegd

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ var boodschappentas = [];
 function gaBoodschappenDoen(){
   var item = document.getElementById("boodschapitem").value;
   boodschappentas.push(item);
-  console.log(boodschappentas);
   toonBoodschappen();
+  document.getElementById("boodschapitem").value = "";
 }
 
 function toonBoodschappen(){
   var htmlString = "";
   for (var x = 0; x < boodschappentas.length; x++) {
-    htmlString += boodschappentas[x] + "<br>";
+    htmlString += boodschappentas[x] + '<input type="button" value="X" onclick="verwijderen()"> <br>';
   }
-
   var deLijst = document.getElementById("lijst");
   deLijst.innerHTML = htmlString;
+}
+
+function verwijderen(){
+  alert("verwijderen");
 }
